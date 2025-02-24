1dbe785710ded8df194007c3f49ca97deddc357b
VikramGujar <vikramgujar300@gmail.com>
2025-02-24 18:50:45 +0530

    Created a Soch AI Project

---
+++ sochai/src/main/java/com/gemini/sochai/SochaiApplication.java
@@ -0,0 +1,13 @@
+package com.gemini.sochai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SochaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SochaiApplication.class, args);
+	}
+
+}
